2e025ef24f601831d3188050d2b74ce5ef7d7dc1
Jaseel <muhammedjaseelk10@gmail.com>
2023-10-18 16:51:38 +0530

    Add project files.

---
+++ dev blogs/Models/Domain/Tag.cs
@@ -0,0 +1,9 @@
+﻿namespace dev_blogs.Models.Domain
+{
+    public class Tag
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+    }
+}
