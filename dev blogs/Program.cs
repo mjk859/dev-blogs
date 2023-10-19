a001f36904edf4933f44135b5b10e9bce8f62ed5
Jaseel <muhammedjaseelk10@gmail.com>
2023-10-19 18:49:40 +0530

    added connection string to database

--- dev blogs/Program.cs
+++ dev blogs/Program.cs
@@ -1,7 +1,12 @@
+using dev_blogs.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<dev_blogsDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("devblogsDbConnectionString")));
 
 var app = builder.Build();
 
