b737db28356e3ad1c66a7d25be8aa731fd63633b
Jaseel <muhammedjaseelk10@gmail.com>
2023-10-19 15:40:37 +0530

    created DbContext class

---
+++ dev blogs/Data/dev blogsDbContext.cs
@@ -0,0 +1,14 @@
+﻿using dev_blogs.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace dev_blogs.Data
+{
+    public class dev_blogsDbContext : DbContext
+    {
+        public dev_blogsDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
