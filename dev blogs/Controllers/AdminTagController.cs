b0a4e67a2fe41cadb23728fae428b04e5443631b
Jaseel <muhammedjaseelk10@gmail.com>
2023-10-21 11:54:49 +0530

    added Tags controller - Admin finctionality to add a tag

---
+++ dev blogs/Controllers/AdminTagController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dev_blogs.Controllers
+{
+    public class AdminTagController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
